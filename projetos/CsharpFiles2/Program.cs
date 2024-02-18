f02edb7eff985ae188438015708307a49b7342b4
hikki <gustavoht2014@gmail.com>
2024-02-18 20:55:41 -0300

    uhul

---
+++ projetos/CsharpFiles2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Collections.Generic;
+namespace CsharpFiles2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\hikki\Desktop\Xadrez";
+
+            try
+            {
+                IEnumerable<string> folders =  Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                foreach(string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                Directory.CreateDirectory(path + "\\newfolder");
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+            
+
+        }
+    }
+}
---
+++ projetos/FilesCsharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+namespace FilesCsharp
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\hikki\Desktop\File1.txt";
+            string targetPath = @"C:\Users\hikki\Desktop\File2.txt";
+
+            try
+            {
+                FileInfo fileinfo = new FileInfo(sourcePath);
+                fileinfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error has ocurred: "+ ex.Message);
+            }
+        }
+    }
+}
